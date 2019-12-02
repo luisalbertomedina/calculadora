7d3be3cf12eec5b441676b5f55a64aa2e3ffacc2
alexisGLez <45187159+alexis2213@users.noreply.github.com>
2019-12-02 16:58:22 -0600

    Update vista.java

--- src/vista/vista.java
+++ src/vista/vista.java
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package MVC.vista;
+package vista;
+
 
 
 public class vista extends javax.swing.JFrame {
