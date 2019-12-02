5ac141ca4a45256c3b47aaa5bf27d2e96b186186
alexisGLez <45187159+alexis2213@users.noreply.github.com>
2019-12-02 16:59:54 -0600

    Update ControladorPrincipal.java

--- src/Controlador/ControladorPrincipal.java
+++ src/Controlador/ControladorPrincipal.java
@@ -3,11 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package MVC;
+package Controlador;
 
 
-import MVC.modelo.modelo;
-import MVC.vista.vista;
+import modelo.modelo;
+import vista.vista;
+
 
 /**
  *
