3bf1e835b2eb9f5f47d287eb7a5f2eaaa02d90af
alexisGLez <45187159+alexis2213@users.noreply.github.com>
2019-12-02 17:03:22 -0600

    Update Token.java

--- src/modelo/Token.java
+++ src/modelo/Token.java
@@ -2,7 +2,7 @@
  * Un token consiste en la posición del recurso y la referencia al símbolo
  * (que contiene el string del operador Ej. "-" y el tipo E. 1). Los tipos de símbolos son constantes.
  */
-package MVC.modelo;
+package modelo;
 
 public class Token {
 
