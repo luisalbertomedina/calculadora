af5e911d48451ef5d1faaab93c3728b2f68798b2
luisalbertomedina <medina.a.luisalberto@gmail.com>
2019-12-03 07:14:43 -0600

    Agregando más Pruebas en Modelo

--- CalculadoraNumerica/test/Modelo/ModeloTest.java
+++ CalculadoraNumerica/test/Modelo/ModeloTest.java
@@ -12,10 +12,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Igniter
- */
+
 public class ModeloTest {
     
     public ModeloTest() {
@@ -41,7 +38,7 @@ public class ModeloTest {
      * Test of calc method, of class Modelo.
      */
     @Test
-    public void testCalc() throws Exception {
+    public void testCalc1() throws Exception {
         System.out.println("calc");
         String expresion = "(5+6)*2-3";
         Modelo instance = new Modelo();
@@ -52,7 +49,69 @@ public class ModeloTest {
         if (expResult!=result) {
             fail("The test case is a prototype.");
         }
-        
     }
-    
+    /**
+     * Test of calc method, of class Modelo.
+     */
+    @Test
+    public void testCalc2() throws Exception {
+        System.out.println("calc");
+        String expresion = "(5-3)^2-3";
+        Modelo instance = new Modelo();
+        double expResult = 1;
+        double result = instance.calc(expresion);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        if (expResult!=result) {
+            fail("The test case is a prototype.");
+        }
+    }
+    /**
+     * Test of calc method, of class Modelo.
+     */
+    @Test
+    public void testCalc3() throws Exception {
+        System.out.println("calc");
+        String expresion = "6+15/3*2-1";
+        Modelo instance = new Modelo();
+        double expResult = 15;
+        double result = instance.calc(expresion);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        if (expResult!=result) {
+            fail("The test case is a prototype.");
+        }
+    }
+    /**
+     * Test of calc method, of class Modelo.
+     */
+    @Test
+    public void testCalc4() throws Exception {
+        System.out.println("calc");
+        String expresion = "2^3+(6/3-2)";
+        Modelo instance = new Modelo();
+        double expResult = 8;
+        double result = instance.calc(expresion);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        if (expResult!=result) {
+            fail("The test case is a prototype.");
+        }
+    }
+    /**
+     * Test of calc method, of class Modelo.
+     */
+    @Test
+    public void testCalc5() throws Exception {
+        System.out.println("calc");
+        String expresion = "(25/(2*5-5))";
+        Modelo instance = new Modelo();
+        double expResult = 5;
+        double result = instance.calc(expresion);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        if (expResult!=result) {
+            fail("The test case is a prototype.");
+        }
+    }
 }
