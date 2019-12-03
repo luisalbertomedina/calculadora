cbe35130d127df9abd43b2d51096038040a0e10e
alexisGLez <45187159+alexis2213@users.noreply.github.com>
2019-12-02 20:25:14 -0600

    Update Interprete.java

--- src/modelo/Interprete.java
+++ src/modelo/Interprete.java
@@ -1,6 +1,9 @@
-/**
- * Clase Interpretador, evalúa la expresión en el árbol
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
+
 package modelo;
 
 public class Interprete {
